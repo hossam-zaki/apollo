17d54510eafd3d3ca75439519469202e3eacd05c
Mohamad <mohamad_abouelafia@brown.edu>
2020-04-06 22:56:53 -0400

    update in recording

--- src/main/java/audioRecording/ProcessRecording.java
+++ src/main/java/audioRecording/ProcessRecording.java
@@ -33,8 +33,8 @@ public class ProcessRecording {
         }
       }
     });
-    recordThread.start();
 
+    recordThread.start();
     try {
       recordThread.wait();
     } catch (InterruptedException e) {
