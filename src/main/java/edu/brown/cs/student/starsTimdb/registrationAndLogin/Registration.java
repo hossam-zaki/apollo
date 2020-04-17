227d50769e91004612410e5f01e8feb5cf095d01
Matan Gans <matan_gans@brown.edu>
2020-04-17 17:27:57 -0400

    added patient registration in database_setup

--- src/main/java/edu/brown/cs/student/starsTimdb/registrationAndLogin/Registration.java
+++ src/main/java/edu/brown/cs/student/starsTimdb/registrationAndLogin/Registration.java
@@ -14,12 +14,15 @@ public class Registration {
 
     PreparedStatement prep;
     try {
-      prep = conn.prepareStatement("INSERT INTO patient VALUES (?, ?, ?, ?s);");
+      prep = conn.prepareStatement("INSERT INTO patient VALUES (?, ?, ?, ?, ?, ?, ?);");
       UUID uuid = UUID.randomUUID();
       prep.setString(1, uuid.toString());
       prep.setString(2, registrationDetails.get(0)); // first name
       prep.setString(3, registrationDetails.get(1)); // last name
       prep.setString(4, registrationDetails.get(2)); // DOB
+      prep.setString(4, registrationDetails.get(3)); // email
+      prep.setString(4, registrationDetails.get(4)); // phone number
+      prep.setString(4, registrationDetails.get(5)); // emergency contact
       prep.addBatch();
       prep.executeBatch();
     } catch (Exception e) { // ask about this
