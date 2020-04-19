03b5480b05797a39069876b81cb59d9f148696f6
Mohamad <mohamad_abouelafia@brown.edu>
2020-04-19 16:06:21 -0400

    addded one junit databs test

--- src/test/java/DatabaseTest.java
+++ src/test/java/DatabaseTest.java
@@ -1,63 +1,51 @@
-package timdb_tests;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import edu.brown.cs.student.maps.commands.MdbCommand;
+import edu.brown.cs.student.starsTimdb.databases.Database;
+import edu.brown.cs.student.starsTimdb.databases.DatabaseSetup;
 
 /**
- * Tester class for mdb command.
+ * Tester class for database package.
  */
-public class MDBTest {
-	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-	private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
-	private final PrintStream originalOut = System.out;
-	private final PrintStream originalErr = System.err;
-  /**
-   * Tester method for mdb command.
-   */
-	@Before
-	public void setUpStreams() {
-	    System.setOut(new PrintStream(outContent));
-	    System.setErr(new PrintStream(errContent));
-	}
+public class DatabaseTest {
+  private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+  private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+  private final PrintStream originalOut = System.out;
+  private final PrintStream originalErr = System.err;
 
-	@After
-	public void restoreStreams() {
-	    System.setOut(originalOut);
-	    System.setErr(originalErr);
-	}
+  @Before
+  public void setUpStreams() {
+    System.setOut(new PrintStream(outContent));
+    System.setErr(new PrintStream(errContent));
+  }
+
+  @After
+  public void restoreStreams() {
+    System.setOut(originalOut);
+    System.setErr(originalErr);
+  }
+
   @Test
-  public void mdbTest() {
-	setUpStreams();
-    MdbCommand mdb = new MdbCommand();
-    List<String> list = new ArrayList<String>();
-    list.add("mdb");
-    list.add("data/timdb/timdb.sqlite3");
-    mdb.executeCommand(list);    
-    assertTrue(outContent.toString().contains("data/timdb/timdb.sqlite3"));
+  public void dbTest() {
+    setUpStreams();
+    Database.makeDatabase();
+    assertTrue(Database.getConn() != null);
     restoreStreams();
 
   }
+
   @Test
-  public void mdbIncorrectTest() {
-	setUpStreams();
-    MdbCommand mdb = new MdbCommand();
-    List<String> list = new ArrayList<String>();
-    list.add("mdb");
-    list.add("awefawefawef");
-    mdb.executeCommand(list);    
-    assertTrue(errContent.toString().contains("ERROR"));
+  public void dbSetupTest() {
+    setUpStreams();
+    DatabaseSetup.createNewDatabase();
+    assertTrue(outContent.toString().contains("Generating Database..."));
     restoreStreams();
 
   }
