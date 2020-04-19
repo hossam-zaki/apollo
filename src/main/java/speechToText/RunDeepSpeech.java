dffb3a62e7f1cebf46c31d6e53ae397ee7d74e3e
hossam-zaki <hossam_zaki@brown.edu>
2020-04-19 14:19:26 -0400

    added stringbuilder for starting deepspeech

--- src/main/java/speechToText/RunDeepSpeech.java
+++ src/main/java/speechToText/RunDeepSpeech.java
@@ -8,15 +8,16 @@ public class RunDeepSpeech {
 	public static void transcribe(String filename) {
 	       Process process;
 		try {
-			StringBuilder str = new StringBuilder("--file ");
+			StringBuilder str = new StringBuilder();
+			str.append("src/main/java/speechToText/mic_vad_streaming.py ");
+			str.append("--model src/main/java/speechToText/output_graph.pbmm ");
+			str.append("--lm src/main/java/speechToText/lm.binary ");
+			str.append("-t src/main/java/speechToText/trie ");
+			str.append("--file ");
 			str.append(filename);
-			
-			process = Runtime.getRuntime().exec("src/main/java/speechToText/mic_vad_streaming.py " +
-					"--model src/main/java/speechToText/output_graph.pbmm "+ "--lm src/main/java/speechToText/lm.binary "+
-					"-t src/main/java/speechToText/trie "+ str.toString());
+			process = Runtime.getRuntime().exec(str.toString());
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.err.println("ERROR: Error with executing the script");
 		}
 	}
 }
