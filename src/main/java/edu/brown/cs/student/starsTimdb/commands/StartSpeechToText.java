07a570176756e6d1bff3476e8216d96a5bb4ccd2
Mohamad <mohamad_abouelafia@brown.edu>
2020-04-19 14:18:43 -0400

    fixed error checking for wav files in command start speech

--- src/main/java/edu/brown/cs/student/starsTimdb/commands/StartSpeechToText.java
+++ src/main/java/edu/brown/cs/student/starsTimdb/commands/StartSpeechToText.java
@@ -1,5 +1,7 @@
 package edu.brown.cs.student.starsTimdb.commands;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.List;
 
 import edu.brown.cs.student.starsTimdb.repl.Executable;
@@ -7,10 +9,21 @@ import speechToText.RunDeepSpeech;
 
 public class StartSpeechToText implements Executable {
 
-	@Override
-	public void executeCommand(List<String> string) {
-		new RunDeepSpeech();
-		RunDeepSpeech.transcribe(string.get(1));
-	}
+  @Override
+  public void executeCommand(List<String> string) {
+    String wavFile = string.get(1);
+    if (!wavFile.endsWith(".wav")) {
+      System.err.println("ERROR: Did not pass in a WAV File");
+      return;
+    }
+    try {
+      FileReader file = new FileReader(wavFile);
+    } catch (FileNotFoundException e1) {
+      System.err.println("ERROR: WAV File does not exist");
+      return;
+    }
+    new RunDeepSpeech();
+    RunDeepSpeech.transcribe(wavFile);
+  }
 
 }
